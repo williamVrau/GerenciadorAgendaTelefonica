39fd78729a47cb0808c6f170cbd2e9f8393b2c05
William Mateus <williammnovais@gmail.com>
2025-04-16 19:03:45 -0300

    Criacao da funcao para criar um contato

--- AgendaTelefonica/src/Main.java
+++ AgendaTelefonica/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         ArrayList<ArrayList> agendas = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         boolean isOn = true;
-        while (isOn){
+        while (isOn) {
             System.out.println("Segue abaixo todas as funcoes do codigo \n" +
                     "0 - Sair do Programa\n" +
                     "1 - Adicionar Novo Mudero de contato\n" +
@@ -15,15 +15,25 @@ public class Main {
                     "Digite o Numero da funcao");
             int option = scanner.nextInt();
             scanner.nextLine();
-            switch (option){
+            switch (option) {
                 case 0:
                     isOn = false;
                     break;
                 case 1:
+                    System.out.println("Digite o Nome do Contatato que voce gostaria de adicionar\n");
+                    String contact = scanner.nextLine();
+                    System.out.println("Digite o numero do Contatato que voce gostaria de adicionar\n");
+                    String number = scanner.nextLine();
+                    agendas.addFirst(new ArrayList<>());
+                    agendas.getFirst().add(contact);
+                    agendas.getFirst().add(1, number);
                     break;
                 case 2:
                     break;
                 case 3:
+                    for (ArrayList i : agendas) {
+                        System.out.println(i);
+                    }
                     break;
                 case 4:
                     break;
@@ -31,5 +41,7 @@ public class Main {
                     break;
                 default:
                     System.out.println("O Numero que voce digitou na Existe");
+            }
+        }
     }
 }
